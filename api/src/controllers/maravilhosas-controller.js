bf433c7561d218356e7684f94fb0a6f526a21277
nanda-oliveira <fernandaoliveiraufpe@gmail.com>
2020-10-17 13:11:47 -0300

    adicionando controller

--- api/src/controllers/maravilhosas-controller.js
+++ api/src/controllers/maravilhosas-controller.js
@@ -1,11 +1,79 @@
+const { request } = require("express")
+const maravilhosas = require("../models/maravilhosas-models")
+
 //Nomes dos métodos para implementação:
 
-//getMaravilhosas
+//getMaravilhosas GET
 
-//getMaravilhosaById
+const getMaravilhosas = (request, response) => {
+    response.status(200).send(maravilhosas)
+}
 
-//addMaravilhosa 
+//getMaravilhosaById GET
 
+const getMaravilhosaById = (request, response) => {
+    const id = request.params.id
+    const maravilhosaID = maravilhosas.find(maravilhosa => maravilhosa.id == id)
+    if (maravilhosaID) {
+        response.status(200).send(maravilhosaID)
+    } else {
+        response.status(404).send("Maravilhosa nao encontrada")
+    }
+}
+
+//addMaravilhosa POST
+
+const addMaravilhosa = (request, response) => {
+    const { name, photo, subtitle, about, phrase, history, addedBy } = request.body
+    const novaMaravilhosa = {
+        id: id,
+        name: name,
+        photo: photo,
+        subtitle: subtitle,
+        about: about,
+        phrase: phrase,
+        history: history,
+        addedBy: addedby
+    }
+
+    maravilhosas.push(addMaravilhosa)
+    response.status(201).send(addMaravilhosa)
+
+}
 //updateMaravilhosa 
+const updateMaravilhosa = (request, response) => {
+    const maravilhosaAtualizada = request.body
+    const id = parseInt(request.params.id)
+    const maravilhosaParaAtualizar = maravilhosas.find(maravilhosa => maravilhosa.id == id)
 
-//deleteMaravilhosa
+    Object.keys(maravilhosaAtualizada).forEach((chave) => {
+        maravilhosaParaAtualizar[chave] = maravilhosaAtualizada[chave]
+    })
+
+    response.status(200).send(maravilhosaParaAtualizar)
+
+
+    //deleteMaravilhosa
+
+    const deleteMaravilhosa = (request, response) => {
+        const { id } = request.params
+        let filtarMaravilhosa = maravilhosas.filter(maravilhosa => {
+            maravilhosas.id === id
+        })[0]
+
+        const index = maravilhosas.indexOf(filtarMaravilhosa)
+
+        maravilhosas.splice(index, 1)
+
+        response.send(maravilhosas)
+
+    }}
+
+    module.exports = {
+        getMaravilhosas,
+        getMaravilhosaById,
+        addMaravilhosa,
+        updateMaravilhosa,
+        deleteMaravilhosa
+
+    }   
--- api/src/routes/maravilhosas-routes.js
+++ api/src/routes/maravilhosas-routes.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const controller = require("../controllers/maravilhosas-controller")
+const cors = require("cors")
 
 //get /maravilhosas
-
-//post /maravilhosas
+router.get("/maravilhosas", cors(), controller.getMaravilhosas)
 
 //get /maravilhosas/id
+router.get("/maravilhosas/:id", cors(), controller.getMaravilhosaById)
+
+//post /maravilhosas
+router.post("/maravilhosas", cors(), controller.addMaravilhosa)
+
 
 //put /maravilhosas/id
+router.put("/maravilhosas/:id", cors(), controller.updateMaravilhosa)
 
 //delete /maravilhosas/id
+router.delete("/maravilhosas/:id", cors(), controller.deleteMaravilhosa)
+
 
 module.exports = router;
+
